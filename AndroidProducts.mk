713ce2a82006c8cee66d9a3a68a36139a54cecc4
Nabil Zaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-03-20 15:01:35 +0600

    santoni: Only CipherOS supported

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
-# LineageOS (crDroid)
+# CipherOS (Lineage + AOSP)
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_santoni.mk
+    $(LOCAL_DIR)/cipher_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_santoni-userdebug \
-    lineage_santoni-user \
-    lineage_santoni-eng
+    cipher_santoni-userdebug \
+    cipher_santoni-user \
+    cipher_santoni-eng
