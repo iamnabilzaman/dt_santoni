3cc274a96da2f92166ae1fe279e55613b72d5003
Nabil Zaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-04-02 13:19:50 +0600

    santoni: Added ProjectBlaze support

--- cipher_santoni.mk
+++ cipher_santoni.mk
@@ -22,20 +22,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 # Inherit from santoni device
 $(call inherit-product, device/xiaomi/santoni/device.mk)
 
-# Inherit some common Cipher stuff.
-$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
+# Inherit some common Blaze stuff
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+BLAZE_MAINTAINER := iamnabilzaman
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_DEVICE := santoni
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
-PRODUCT_NAME := cipher_santoni
+PRODUCT_NAME := blaze_santoni
 BOARD_VENDOR := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Cipher Flags
-TARGET_BOOT_ANIMATION_RES := 720
-CIPHER_GAPPS := true
-CIPHER_MAINTAINER := iamnabilzaman
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
