9728f86ec40293e5912e483b9490ba5f8da9f500
Nabil Zaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-04-06 19:13:46 +0600

    wfdshim

--- shims/wfdshim.cpp
+++ shims/wfdshim.cpp
@@ -1,3 +1,7 @@
 extern "C" {
    void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb() {}
+   void _ZN6SkData16DummyReleaseProcEPKvPv() {}
+   void _ZN8SkBitmapC1Ev() {}
+   void _ZN8SkBitmap11allocPixelsERK11SkImageInfo() {}
+   void _ZN7android11MotionEvent10initializeEiijiNSt3__15arrayIhLm32EEEiiiiiiNS_20MotionClassificationEffffffffllmPKNS_17PointerPropertiesEPKNS_13PointerCoordsE() {}
 } 
